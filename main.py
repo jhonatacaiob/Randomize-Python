558fc18510340356249ed55d21fee2d37fa2308e
unknown <jhonatacaiob@gmail.com>
2021-12-09 18:44:01 -0300

    adcionado uma função auxiliar para enviar mensagens

--- main.py
+++ main.py
@@ -13,26 +13,29 @@ async def on_ready():
 @bot.command()
 async def add(ctx, arg):
     metodos.add_members(ctx.message.mentions)
-    await ctx.message.channel.send(content = metodos.show_list("Grupo", metodos.group(ctx.guild.id)))
+    await send(ctx.channel, content = metodos.show_list("Grupo", metodos.group(ctx.guild.id)))
 
 @bot.command()
 async def remove(ctx, arg):
     metodos.remove_members(ctx.message.mentions)
-    await ctx.message.channel.send(content = metodos.show_list("Grupo", metodos.group(ctx.guild.id)))        
+    await send(ctx.channel, content = metodos.show_list("Grupo", metodos.group(ctx.guild.id)))        
 
 @bot.command()
 async def group(ctx):
-    await ctx.message.channel.send(content = metodos.show_list("Grupo", metodos.group(ctx.guild.id)))
+    await send(ctx.channel, metodos.show_list("Grupo", metodos.group(ctx.guild.id)))
 
 @bot.command(name = "remove-all")
 async def remove_all(ctx):
     metodos.remove_all(ctx.guild.id)
-    await ctx.message.channel.send(content = metodos.show_list("Grupo", metodos.group(ctx.guild.id)))        
+    await send(ctx.channel, metodos.show_list("Grupo", metodos.group(ctx.guild.id)))        
 
 @bot.command()
 async def randomize(ctx):  
-    await ctx.message.channel.send(content = metodos.randomize(ctx.guild.id))
+    await send(ctx.channel, metodos.randomize(ctx.guild.id))
 
+
+async def send(channel, content):
+    await channel.send(content = content)
 
 
 bot.run(config('DISCORD_TOKEN'))
